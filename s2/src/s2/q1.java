ec64ddab69fe0d7ca6cb59dae14dc8af267f6308
shayanah <shawn.hossini@gmial.com>
2015-01-24 11:49:34 -0500

    vresion commit

---
+++ s2/src/s2/q1.java
@@ -0,0 +1,22 @@
+package s2;
+
+import java.util.Scanner;
+
+public class q1 {
+
+	public static void main(String [] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter your Username: ");
+		String username = in.next();
+	
+		
+		System.out.print("Enter your Password: ");
+		String password = in.next();
+		
+		
+		System.out.println("Hello " +  username +"," + 
+		"Welcome to CSC200 class! and Your password is " + password);
+		
+	}
+}
